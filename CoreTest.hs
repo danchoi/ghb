e0f6f6f71cbc74b43a84be6bd216999941d45168
Daniel Choi <dhchoi@gmail.com>
2012-12-29 11:07:32 -0500

    import fromJust

--- CoreTest.hs
+++ CoreTest.hs
@@ -2,11 +2,12 @@ module Main where
 import Data.Aeson
 import GHCore
 import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Maybe (fromJust)
 
 main = do
   raw <- readFile "json/user.json"
   let json = B.pack raw
   let r = decode json :: Maybe User
-  putStrLn . show $ r
+  putStrLn . show $ fromJust r
 
 
