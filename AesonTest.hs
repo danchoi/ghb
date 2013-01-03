740ce7fae5da96a832f9234294fb4845667b74ad
Daniel Choi <dhchoi@gmail.com>
2013-01-03 11:11:54 -0500

    try converting map to list

--- AesonTest.hs
+++ AesonTest.hs
@@ -9,6 +9,6 @@ main = do
   raw <- readFile "json/issues.json"
   let json = B.pack raw
   let r = decode json :: Maybe ([M.Map String Value])
-  mapM_ (putStrLn . show) $ fromJust r
+  mapM_ (putStrLn . show . M.toList) $ fromJust r
 
 
