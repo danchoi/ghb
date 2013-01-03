98d807b125682dd4ca2396a9cf2ec09a4409e5ea
Daniel Choi <dhchoi@gmail.com>
2013-01-03 11:08:08 -0500

    parse json into a generic Value

---
+++ AesonTest.hs
@@ -0,0 +1,14 @@
+module Main where
+import Data.Aeson
+import GHCore
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Maybe (fromJust)
+import qualified Data.Map as M
+
+main = do
+  raw <- readFile "json/issues.json"
+  let json = B.pack raw
+  let r = decode json :: Maybe ([M.Map String Value])
+  mapM_ (putStrLn . show) $ fromJust r
+
+
