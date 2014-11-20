a155166a443f62666a0b112d65a57a27b2c8ed82
Daniel Choi <dhchoi@gmail.com>
2014-11-20 10:43:04 -0500

    simplify types and fix FromJSON

--- Ghb/Core.hs
+++ Ghb/Core.hs
@@ -10,39 +10,20 @@ import qualified Data.Text.Lazy.Encoding as TE
 import Data.Time.Clock
 import qualified Data.Vector as V
 
-
-data User = User { userLogin :: String } deriving (Show)
-
 data IssueState = Open | Closed deriving (Show)
 
-data PullRequest = PullRequest {
-    pullRequestPatchUrl :: Maybe String
-  , pullRequestDiffUrl :: Maybe String
-  , pullRequestHtmlUrl :: Maybe String
-  } deriving (Show)
-
 data Issue = Issue {
-    issueId :: Int
-  , issueNumber :: Int
+    issueNumber :: Int
+  , issueTitle :: String
   , issueNumComments :: Int
   , issueState :: IssueState
   , issueUrl :: String
-  , issueCommentsUrl :: String
-  , issueTitle :: String
   , issueBody :: String
-  , issueMilestone :: Maybe Int
   , issueCreated :: String
   , issueUpdated :: String
-  , issuePullRequest :: PullRequest
-  , issueUser :: User
-  , issueAssignee :: Maybe User
-  , issueLabels :: [Label]
-  } deriving (Show)
-
-data Label = Label {
-    labelColor :: String
-  , labelName :: String
-  , labelUrl :: String
+  , issueUsername :: Text
+  , issueAssignee :: Maybe Text
+  , issueLabels :: [Text]
   } deriving (Show)
 
 data Comment = Comment {
@@ -52,22 +33,8 @@ data Comment = Comment {
   , commentBody :: String
   , commentCreated :: String -- change to date
   , commentUpdated :: String
-  , commentUser :: User
+  , commentUser :: Text
   } deriving (Show)
-
-
--- FromJSON instances
-
-instance FromJSON User where
-    parseJSON (Object v) = 
-        User <$> v .: "login" 
-
-instance FromJSON PullRequest where
-    parseJSON (Object v) =
-        PullRequest <$>
-            v .: "patch_url" <*>
-            v .: "diff_url" <*>
-            v .: "html_url" 
 
 instance FromJSON IssueState where
     parseJSON (String "open") = pure Open
@@ -75,29 +42,19 @@ instance FromJSON IssueState where
 
 instance FromJSON Issue where
     parseJSON (Object v) =
-        Issue <$>
-            v .: "id" <*>
-            v .: "number" <*>
-            v .: "comments" <*>
-            v .: "state" <*>
-            v .: "url" <*>
-            v .: "comments_url" <*>
-            v .: "title" <*>
-            v .: "body" <*>
-            v .: "milestone" <*>
-            v .: "created_at" <*>
-            v .: "updated_at" <*>
-            v .: "pull_request" <*>
-            v .: "user" <*>
-            v .: "assignee" <*>
-            v .: "labels"
-
-instance FromJSON Label where
-    parseJSON (Object v) = 
-        Label <$>
-            v .: "color" <*>
-            v .: "name" <*>
-            v .: "url" 
+        Issue 
+          <$> v .: "number" 
+          <*> v .: "title" 
+          <*> v .: "comments" 
+          <*> v .: "state" 
+          <*> v .: "url" 
+          <*> v .: "body" 
+          <*> v .: "created_at" 
+          <*> v .: "updated_at" 
+          <*> (v .: "user" >>= (.: "login")) 
+          <*> ((v .: "assignee" >>= (.: "login"))  <|> pure Nothing )
+          <*> (v .: "labels" >>= mapM (.: "name"))
+  
 
 instance FromJSON Comment where 
     parseJSON (Object v) = 
@@ -110,26 +67,5 @@ instance FromJSON Comment where
             v .: "updated_at" <*>
             v .: "user" 
 
-
-
-{-
-
-
-  {"type": "User",
-           "followers_url": "https://api.github.com/users/danchoi/followers",
-           "login": "danchoi",
-           "starred_url": "https://api.github.com/users/danchoi/starred{/owner}{/repo}",
-           "gists_url": "https://api.github.com/users/danchoi/gists{/gist_id}",
-           "following_url": "https://api.github.com/users/danchoi/following",
-           "organizations_url": "https://api.github.com/users/danchoi/orgs",
-           "repos_url": "https://api.github.com/users/danchoi/repos",
-           "events_url": "https://api.github.com/users/danchoi/events{/privacy}",
-           "url": "https://api.github.com/users/danchoi",
-           "subscriptions_url": "https://api.github.com/users/danchoi/subscriptions",
-           "received_events_url": "https://api.github.com/users/danchoi/received_events",
-           "id": 12854, "gravatar_id": "004e78a7e0aaf9f1eb0009668cd84b1a",
-           "avatar_url": "https://secure.gravatar.com/avatar/004e78a7e0aaf9f1eb0009668cd84b1a?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"}
-
--}
 
 
--- Ghb/CoreTest.hs
+++ Ghb/CoreTest.hs
@@ -5,18 +5,13 @@ import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Maybe (fromJust, fromMaybe)
 import System.Environment
 
-testUser = do
-  raw <- readFile "json/user.json"
-  let json = B.pack raw
-  let r = decode json :: Maybe User
-  putStrLn . show $ fromJust r
-
 testIssue = do 
     [f] <- getArgs
     raw <- fmap B.pack $ readFile f
-    B.putStrLn raw
-    let r = decode raw :: Maybe [Issue]
-    mapM_ print $ fromMaybe [] r
+    let r = eitherDecode raw :: Either String [Issue]
+    case r of 
+      Left err -> putStrLn err
+      Right xs -> mapM_ print xs
 
 
 
